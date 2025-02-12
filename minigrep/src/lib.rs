c0927a82e163f63542d890c122ad9067a319391c
Noah Whited <52642358+noahboa27@users.noreply.github.com>
2025-02-11 17:14:08 -0700

    practice

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,3 +1,11 @@
+//! # MiniGrep
+//!
+//! `minigrep` is a program that will search a text file for a string
+//! and print the lines in which they occur.
+//!
+//! # Example usage
+//! `minigrep sweet poem.txt`
+
 use std::env;
 use std::error::Error;
 use std::fs;
@@ -9,13 +17,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
@@ -47,28 +60,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 // slices from contents
 // data returned will live as long as the contents argument
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 #[cfg(test)]
